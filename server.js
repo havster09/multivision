d60a0d0d32f4e6ed6ff1459b6a75c25e59a2f1e2
Haven.Ramos <havster09@gmail.com>
2015-07-08 10:14:07 +1000

    skeleton set

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     stylus = require('stylus'),
     morgan = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -20,11 +21,36 @@ app.use(stylus.middleware(
     }
 ));
 app.use(express.static(__dirname+'/public'));
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+mongoose.connect('mongodb://localhost/multivision');
+
+var db = mongoose.connection;
+
+db.on('error',console.error.bind(console,'connection error'));
+
+db.once('open',function callback(){
+    console.log('multivision db opened');
+});
+
+var messageSchema = mongoose.Schema({message:String});
+
+var Message = mongoose.model('Message',messageSchema);
+var mongoMessage;
+
+Message.findOne().exec(function(err,messageDoc){
+    mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath',function(req,res){
+    res.render('partials/'+req.params.partialPath);
+});
+
 app.get('*',function(req,res){
-    res.render('index');
+    res.render('index',{
+        mongoMessage:mongoMessage
+    });
 });
 
 var port = 3030;
@@ -32,3 +58,4 @@ var port = 3030;
 app.listen(port,function(){
    console.log('listening to this port '+port);
 });
+
